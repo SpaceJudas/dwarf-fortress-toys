a87eb8ce17439415aa4c90d59204e0d33bd5e720
Spencer Ewall <spencerjewall@gmail.com>
2015-10-01 22:53:24 -0700

    lots of stuff

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var data = require('./routes/data');
+var itemd3 = require('./routes/itemd3');
+
 
 var app = express();
 
@@ -15,15 +17,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/data', data);
+app.use('/itemd3', itemd3);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
