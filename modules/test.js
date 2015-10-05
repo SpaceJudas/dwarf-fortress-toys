e2530f14a06f9010cf244500fbb7b8e4149f8635
Spencer Ewall <spencerjewall@gmail.com>
2015-10-05 14:42:40 -0700

    strigify!

--- modules/test.js
+++ modules/test.js
@@ -3,5 +3,5 @@ var sankey = require('./sankeyData.js');
 //'Category:DF2014:Items'
 //'Category:DF2014:Workshops'
 sankey.SankeyData('Category:DF2014:Items', function(err, data) {
-  console.log(data);
+  console.log(JSON.stringify(data));
 });
