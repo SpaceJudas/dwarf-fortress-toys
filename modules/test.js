8455213d679233b806ee10f7ef25907d6a7487fc
Spencer Ewall <spencerjewall@gmail.com>
2015-10-06 18:15:23 -0700

    adding dwarf fortress objects folder

--- modules/test.js
+++ modules/test.js
@@ -1,7 +1,29 @@
 var sankey = require('./sankeyData.js');
+var _ = require('underscore');
 
 //'Category:DF2014:Items'
 //'Category:DF2014:Workshops'
 sankey.SankeyData('Category:DF2014:Items', function(err, data) {
-  console.log(JSON.stringify(data));
+  countLeaves(data);
 });
+
+function countLeaves(data) {
+  var linkDict = {};
+  _.each(data.links, function(e) {
+    if (!linkDict[e.source]) {
+      linkDict[e.source] = {};
+    }
+    if (!linkDict[e.source][e.target]) {
+      linkDict[e.source][e.target] = {};
+    }
+    linkDict[e.source][e.target] = e;
+  });
+
+  var leafCount = 0;
+  _.each(data.nodes, function(e, i) {
+    if (!linkDict[i]) {
+      leafCount++;
+    }
+  });
+  console.log(leafCount);
+}
